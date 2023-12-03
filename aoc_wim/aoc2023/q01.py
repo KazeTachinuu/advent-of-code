2d4e65a473d69be186b8e752b00de57963e41b2d
Hugo Sibony <hugo.sibony@epita.fr>
2023-12-03 02:56:37 +0100

    remove previous years

--- aoc_wim/aoc2023/q01.py
+++ aoc_wim/aoc2023/q01.py
@@ -3,18 +3,26 @@
 https://adventofcode.com/2023/day/1
 """
 import regex as re
-from aocd import data
+from aocd import data, submit
+
 
 ns = "one, two, three, four, five, six, seven, eight, nine".split(", ")
 d = dict(zip(ns, "123456789"))
 a = b = 0
-pat_a = re.compile(r"\d")
 pat_b = re.compile(rf"(\d|{'|'.join(ns)})")
 for line in data.splitlines():
-    nums_a = [int(x) for x in pat_a.findall(line)] or [0]
+
+    nums_a = []
+    for c in line:
+        if c in "0123456789":
+            nums_a.append(int(c))
+
     nums_b = [int(d.get(x, x)) for x in pat_b.findall(line, overlapped=True)] or [0]
     a += nums_a[0] * 10 + nums_a[-1]
     b += nums_b[0] * 10 + nums_b[-1]
 
 print("answer_a:", a)
 print("answer_b:", b)
+submit(a, part="a", day=1, year=2023)
+submit(b, part="b", day=1, year=2023)
+
